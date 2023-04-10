62df1def0c00d75791019ec2115c5682747e3fa8
Yoon Koh <ykoh2@chicagobooth.edu>
2023-04-10 02:20:03 +0000

    progress

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,9 +4,28 @@ class ApplicationController < ActionController::Base
   # Add your actions below this line
   # ================================
 
-  def play_rock
-    #write a ton of code
+  def homepage
+    render({ :template => "game_templates/rules.html.erb" })
+  end
 
+  def play_rock
     render({ :template => "game_templates/user_rock.html.erb" })
   end
+
+  def play_paper
+    @comp_move = ["rock", "paper", "scissors"].sample
+
+    if @comp_move == "rock"
+      @outcome = "won"
+    elsif @comp_move =="paper"
+      @outcome = "tied"
+    elsif @comp_move =="scissors"
+      @outcome = "lost"
+    end
+
+
+
+    render({ :template => "game_templates/user_paper.html.erb" })
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  get("/rock", {:controller => "application", :action => "play_rock"})
+
+  get("/", { :controller => "application", :action => "homepage" })
+
+  get("/rock", { :controller => "application", :action => "play_rock" })
+
+  get("/paper", { :controller => "application", :action => "play_paper" })
+
 end
