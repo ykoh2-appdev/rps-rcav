4b45066f849c371927b42c43c5f83efa96de915c
Yoon Koh <ykoh2@chicagobooth.edu>
2023-04-10 01:57:45 +0000

    rock page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,9 @@ class ApplicationController < ActionController::Base
   # Add your actions below this line
   # ================================
 
+  def play_rock
+    #write a ton of code
+
+    render({ :template => "game_templates/user_rock.html.erb" })
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,3 @@
 Rails.application.routes.draw do
-
-
+  get("/rock", {:controller => "application", :action => "play_rock"})
 end
