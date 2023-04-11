a50fc770dff1cc4f30032c4626c1db02d31264fd
Yoon Koh <ykoh2@chicagobooth.edu>
2023-04-11 14:33:20 +0000

    done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  layout(false)
+  layout("wrapper.html.erb")
 
   # Add your actions below this line
   # ================================
@@ -9,6 +9,15 @@ class ApplicationController < ActionController::Base
   end
 
   def play_rock
+    @comp_move = ["rock", "paper", "scissors"].sample
+
+    if @comp_move == "rock"
+      @outcome = "tied"
+    elsif @comp_move == "paper"
+      @outcome = "lost"
+    elsif @comp_move == "scissors"
+      @outcome = "won"
+    end
     render({ :template => "game_templates/user_rock.html.erb" })
   end
 
@@ -17,15 +26,24 @@ class ApplicationController < ActionController::Base
 
     if @comp_move == "rock"
       @outcome = "won"
-    elsif @comp_move =="paper"
+    elsif @comp_move == "paper"
       @outcome = "tied"
-    elsif @comp_move =="scissors"
+    elsif @comp_move == "scissors"
       @outcome = "lost"
     end
-
-
-
     render({ :template => "game_templates/user_paper.html.erb" })
   end
 
+  def play_scissors
+    @comp_move = ["rock", "paper", "scissors"].sample
+
+    if @comp_move == "rock"
+      @outcome = "lost"
+    elsif @comp_move == "paper"
+      @outcome = "won"
+    elsif @comp_move == "scissors"
+      @outcome = "tied"
+    end
+    render({ :template => "game_templates/user_scissors.html.erb" })
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-
   get("/", { :controller => "application", :action => "homepage" })
 
   get("/rock", { :controller => "application", :action => "play_rock" })
 
   get("/paper", { :controller => "application", :action => "play_paper" })
 
+  get("/scissors", { :controller => "application", :action => "play_scissors" })
 end
